72d374361def3ea2796df5dcf0574a41aa927e81
s-omranpour <soroush.333@gmail.com>
2021-10-04 18:34:50 +0330

    const modified + event finalized

--- deepmusic/modules/const.py
+++ deepmusic/modules/const.py
@@ -1,6 +1,31 @@
 import numpy as np
 import itertools
 
+PITCHES = np.arange(0, 128)
+PIRCH_CLASSES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+CHORD_QUALITIES = ['M', 'm', 'o', '+', '7', 'M7', 'm7', 'o7', '/o7', 'sus2', 'sus4']
+CHORDS = ['_'.join(e) for e in itertools.product(PIRCH_CLASSES, CHORD_QUALITIES)]
+INSTRUMENT_PROGRAMS = np.arange(0, 128)
+INSTRUMENT_FAMILIES = [
+    'piano',
+    'percussion',
+    'organ',
+    'guitar',
+    'bass',
+    'strings',
+    'ensemble',
+    'brass',
+    'reed',
+    'pipe',
+    'synth-lead',
+    'synth-pad',
+    'synth-effects',
+    'ethnic',
+    'percussive',
+    'sound-effects',
+    'drums'
+]
+
 class Constants:
     def __init__(
         self, 
@@ -9,7 +34,9 @@ class Constants:
         min_tempo=30,
         max_tempo=300,
         num_tempo_bins=30, 
-        num_velocity_bins=30):
+        num_velocity_bins=30, 
+        use_inst_family=False, 
+        ):
 
         ## time
         self.unit = unit
@@ -29,35 +56,6 @@ class Constants:
         self.num_tempo_bins = num_tempo_bins
         self.tempo_bins = np.linspace(min_tempo, max_tempo, num_tempo_bins, dtype=int)
 
-        ## chord
-        self.pitch_classes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-        self.chord_qualities = ['M', 'm', 'o', '+', '7', 'M7', 'm7', 'o7', '/o7', 'sus2', 'sus4']
-        self.chords = ['_'.join(e) for e in itertools.product(self.pitch_classes, self.chord_qualities)]
-
-        ## instrument
-        self.instruments = [
-            'piano',
-            'percussion',
-            'organ',
-            'guitar',
-            'bass',
-            'strings',
-            'ensemble',
-            'brass',
-            'reed',
-            'pipe',
-            'synth-lead',
-            'synth-pad',
-            'synth-effects',
-            'ethnic',
-            'percussive',
-            'sound-effects',
-            'drums'
-        ]
-
-        ## pitch
-        self.pitches = np.arange(0, 128)
-
         ## duration
         self.duration_bins = np.arange(1, 4*unit+1)
 
@@ -66,11 +64,12 @@ class Constants:
         self.velocity_bins = np.linspace(1, 127, num_velocity_bins, dtype=int)
 
         ## REMI tokens
+        self.use_inst_family = use_inst_family
         self.all_tokens = ['Bar', 'EOS', 'MASK'] +\
             ['BeatPosition_'+str(p) for p in self.position_bins]+ \
                 ['BeatTempo_'+str(t) for t in self.tempo_bins]+ \
                     ['BeatChord_'+str(c) for c in self.chords] + \
-                        ['NoteInstFamily_'+str(inst) for inst in self.instruments] + \
+                        ['NoteInstrumentFamily_'+str(inst) for inst in INSTRUMENT_FAMILIES] if use_inst_family else ['NoteInstrument_'+str(inst) for inst in INSTRUMENT_PROGRAMS] + \
                             ['NotePitch_'+str(i) for i in self.pitches] + \
                                 ['NoteDuration_'+str(d) for d in self.duration_bins] + \
                                     ['NoteVelocity_'+str(v)for v in self.velocity_bins]
@@ -86,10 +85,11 @@ class Constants:
 
     def __eq__(self, o: object):
         if isinstance(o, Constants):
-            return self.unit == o.unit and\
-                self.tick_resol == o.tick_resol and \
-                    self.min_tempo == o.min_tempo and \
-                        self.max_tempo == o.max_tempo and \
-                            self.num_tempo_bins == o.num_tempo_bins and \
-                                self.num_velocity_bins == o.num_velocity_bins
+            return self.use_inst_family == o.use_inst_family and \
+                self.unit == o.unit and\
+                    self.tick_resol == o.tick_resol and \
+                        self.min_tempo == o.min_tempo and \
+                            self.max_tempo == o.max_tempo and \
+                                self.num_tempo_bins == o.num_tempo_bins and \
+                                    self.num_velocity_bins == o.num_velocity_bins
         return False
---
+++ deepmusic/modules/event.py
@@ -0,0 +1,118 @@
+from operator import pos
+from typing import List, Dict
+import numpy as np
+
+from .const import Constants, CHORDS
+
+
+class MusicEvent:
+
+    """
+    *** extended compund word representation ***
+
+        position: 0~len(positions)-1  (0 = stating of a bar) 
+        tempo : 0~len(tempo_bins)  (0 = IGNORE, 1,... show index of value in tempo bins)
+        chord : 0~132 (0 = IGNORE, 1 to 132 show index of name in chords)
+        note_pitch : 0~128 (0 = IGNORE, 1 to 128 show midi pitch + 1)
+        note_duration : 0~len(positions)-1 (0 = IGNORE, 1,... show duration in number of subbeats)
+        note_velocity : 0~len(velocity_bins) (0 = IGNORE, 1,... show index of the value in velocity bins)
+    """
+
+    def __init__(self, 
+        position : int = 0, 
+        tempo : int = 0, 
+        chord : int = 0,
+        note_pitch : int = 0,
+        note_duration : int = 0,
+        note_velocity : int = 0, 
+        const : Constants = None):
+
+        self.const = Constants() if const is None else const
+        assert 0 <= position < self.const.n_bar_steps
+        assert 0 <= tempo < self.const.num_velocity_bins
+        assert 0 <= chord < len(CHORDS)
+        assert 0 <= note_pitch < 128
+        assert 0 <= note_duration < self.const.n_bar_steps
+        assert 0 <= note_velocity < self.const.num_velocity_bins
+
+        self.position = position
+        self.tempo = tempo
+        self.chord = chord
+        self.note_pitch = note_pitch
+        self.note_duration = note_duration
+        self.note_velocity = note_velocity
+
+    @staticmethod
+    def from_cp(cp, const : Constants = None):
+        return MusicEvent(*cp, const=const)
+
+    @staticmethod
+    def from_remi(remi : str, const : Constants = None):
+        tokens = remi.split()
+        pos = 0 if tokens[0] == 'Bar' else int(tokens[0][11:])
+        values = [0 for _ in range(6)]
+        idx = 1
+        for i, attr in enumerate(['Tempo', 'Chord', 'NotePitch', 'NoteDuration', 'NoteVelocity']):
+            if idx >= len(tokens):
+                break
+            if tokens[idx].startswith(attr):
+                values[i] = int(tokens[idx][len(attr):])
+                idx += 1
+            else:
+                values[i] = 0
+        return MusicEvent(pos, *values)
+
+    def __repr__(self, pretty=False):
+        cls = 'Bar' if self.position == 0 else 'Beat'
+        prop = ['position=' + str(self.position)]
+        if self.tempo:
+            prop += ["tempo=" + str(self.const.tempo_bins[self.tempo]) if pretty else str(self.tempo)]
+        if self.chord:
+            prop += ['chord=' + CHORDS[self.chord] if pretty else str(self.chord)]
+        if self.note_pitch:
+            prop += ["note_pitch=" + str(self.note_pitch)]
+        if self.note_duration:
+            prop += ['note_duration=' + str(self.note_duration)]
+        if self.note_velocity:
+            prop += ['note_velocity=' + str(self.const.velocity_bins[self.note_velocity]) if pretty else str(self.note_velocity)] 
+        return f"{cls}({', '.join(prop)})"
+
+    def __eq__(self, other):
+        if isinstance(other, MusicEvent):
+            return self.position == other.position and\
+                self.tempo == other.tempo and\
+                    self.chord == other.chord and\
+                        self.note_pitch == other.note_pitch and\
+                            self.note_duration == other.note_duration and\
+                                self.note_velocity == other.note_velocity
+        return False
+
+    def __hash__(self):
+        return hash((self.position, self.tempo, self.chord, self.note_pitch, self.note_duration, self.note_velocity))
+
+    def change_const(self, const: Constants):
+        if const is not None and self.const != const:
+            self.position = int(np.round(self.position * const.n_bar_steps / self.const.n_bar_steps))
+            if self.tempo > 0:
+                self.tempo = np.argmin(np.abs(const.tempo_bins - self.const.tempo_bins[self.tempo]))
+            self.note_duration = int(np.round(self.note_duration * const.n_bar_steps / self.const.n_bar_steps))
+            if self.note_velocity > 0:
+                self.note_velocity = np.argmin(np.abs(const.velocity_bins - self.const.velocity_bins[self.note_velocity]))
+            self.const = const
+
+    def to_cp(self):
+        return [self.position, self.tempo, self.chord, self.note_pitch, self.note_duration, self.note_velocity]
+
+    def to_remi(self, to_list=True):
+        res = ['Bar' if self.position == 0 else 'BeatPosition' + str(self.position)]
+        if self.tempo > 0:
+            res += ['Tempo'+str(self.tempo)]
+        if self.chord > 0:
+            res += ['Chord' + str(self.chord)]
+        if self.note_pitch > 0:
+            res += [
+                'NotePitch' + str(self.note_pitch),
+                'NoteDuration' + str(self.note_duration),
+                'NoteVelocity' + str(self.note_velocity)
+            ]
+        return res if to_list else ' '.join(res)
